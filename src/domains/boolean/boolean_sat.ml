531d3d02a783236a4c1b99f0dbd1877f6ff230ff
Pierre Talbot <ptalbot@hyc.io>
2019-06-25 18:01:25 +0200

    [Boolean][SAT] WIP on the closure function.

--- src/domains/boolean/boolean_sat.ml
+++ src/domains/boolean/boolean_sat.ml
@@ -1,1 +1,148 @@
 open Satml
+
+module type Boolean_sat_sig =
+sig
+  type t
+
+  module R = Boolean_rep
+
+  (** Boolean are representable on integers.
+      NOTE: We use `Bound_int` instead of introducing a new `Bound_bool`. *)
+  module B = Bound_int
+
+  val empty: t
+  val extend: t -> R.var_kind -> (t * R.var_id)
+  val project: t -> R.var_id -> (B.t * B.t)
+  val lazy_copy: t -> int -> t list
+  val copy: t -> t
+  val closure: t -> t
+  val weak_incremental_closure: t -> R.rconstraint -> t
+  val entailment: t -> R.rconstraint -> Kleene.t
+  val split: t -> t list
+  val volume: t -> float
+  val state_decomposition: t -> Kleene.t
+  val print: R.t -> Format.formatter -> t -> unit
+end
+
+module Boolean_sat =
+struct
+  (* Unfortunately, minisatml relies on a global variable.
+     We should update it later. *)
+  type t = {
+    (* The depth is necessary to backtrack the state of minisat, it corresponds to the decision level. *)
+    depth: int;
+  }
+
+  module R = Boolean_rep
+
+  (** Boolean are representable on integers.
+      NOTE: We use `Bound_int` instead of introducing a new `Bound_bool`. *)
+  module B = Bound_int
+
+  let empty = { depth=0 }
+
+  (* Minisatml does not use functional structure for automatic backtracking, so we trigger the backtracking manually.
+     For safety, we guard all public functions of this module with `backtrack_state`.
+     It avoids creating a new function such as `backtrack` only for this abstract domain.
+     If later, we notice that such a function is useful for other domain, we might add it to `Abstract_domain`. *)
+  let backtrack_state b =
+    if Solver.decisionLevel () <> b.depth then
+      Solver.cancelUntil b.depth
+
+  let extend b () =
+    backtrack_state b;
+    (b, Solver.newVar ())
+
+  let project b v =
+    backtrack_state b;
+    match Solver.value v with
+    | LTrue -> (B.one, B.one)
+    | LFalse -> (B.zero, B.zero)
+    | LUndef -> (B.zero, B.one)
+
+  let lazy_copy b n =
+    backtrack_state b;
+    Solver.newDecisionLevel ();
+    let b = {depth=Solver.decisionLevel ()} in
+    List.init n (fun _ -> b)
+
+  let copy b = failwith "`copy` is not supported on `Boolean_sat`."
+
+  (* The learnt clause are stored in the watched literals array.
+     Actually, there is no explicit "learnt clause database". *)
+  let learn_clause clause =
+    let clause = Clause.clause_new (Vec.get_data clause) (Vec.size clause) ~learnt:true in
+    attachClause clause;
+    uncheckedEnqueue_clause (Vec.get clause 0) clause;
+    varDecayActivity ()
+
+  (* This closure extracts the propagation and conflict resolution parts from `Solver.search` in minisatml. *)
+  let closure b =
+    backtrack_state b;
+    let conflict_clause = ref (Solver.propagate ()) in
+    (* In case of a conflict, we compute the backjump point and register the learnt clause. *)
+    if !conflict_clause <> Solver.dummy_clause then
+      if decisionLevel () = 0 then raise Bot.Bot_found;
+      let learnt_clause = Vec.init 0 dummy_lit in
+      let backtrack_level = ref 0 in
+      analyze conflict_clause learnt_clause backtrack_level;
+      learn_clause learnt_clause;
+      raise (Conflict !backtrack_level)
+    end
+    else begin
+      (* No Conflict *)
+      let next = ref dummy_lit in
+      begin
+        try
+          while decisionLevel() < Vec.size env.assumptions do
+            (* Perform user provided assumption. *)
+            let p = Vec.get env.assumptions (decisionLevel ()) in
+            if value p = Lbool.LTrue then
+              (* Dummy decision level: *)
+              newDecisionLevel ()
+            else if value p = Lbool.LFalse then begin
+              analyzeFinal (Lit.tild p) env.conflict;
+              raise (Search Lbool.LFalse)
+            end else begin
+              next := p;
+              raise Break
+            end
+          done
+        with Break -> ()
+        end;
+        if !next = dummy_lit then begin
+          (* New variable decision: *)
+          env.decisions <- env.decisions + 1;
+          next := pickBranchLit env.polarity_mode env.random_var_freq;
+          if !next = dummy_lit then
+            (* Model found *)
+            raise (Search Lbool.LTrue)
+      end;
+      (* Increase decision level and enqueue next *)
+      assert (value !next = Lbool.LUndef);
+      newDecisionLevel ();
+
+      uncheckedEnqueue !next;
+    end
+  done;
+  Lbool.LUndef
+with Search b -> b
+
+  let weak_incremental_closure = : t -> R.rconstraint -> t
+    backtrack_state b;
+
+  let entailment = : t -> R.rconstraint -> Kleene.t
+    backtrack_state b;
+
+  let split = : t -> t list
+    backtrack_state b;
+
+  let volume = : t -> float
+    backtrack_state b;
+
+  let state_decomposition = : t -> Kleene.t
+    backtrack_state b;
+
+  let print = : R.t -> Format.formatter -> t -> unit
+    backtrack_state b;
+end
--- src/domains/boolean/boolean_sat.mli
+++ src/domains/boolean/boolean_sat.mli
@@ -1,57 +1,25 @@
-open Satml
-
-module type Boolean_sat =
+module type Boolean_sat_sig =
 sig
   type t
-  module I: Vardom_sig.Vardom_sig
+
   module R = Boolean_rep
-  type itv = I.t
-  type bound = I.B.t
-  (** Creates an empty box. *)
+
+  (** Boolean are representable on integers.
+      NOTE: We use `Bound_int` instead of introducing a new `Bound_bool`. *)
+  module B = Bound_int
+
   val empty: t
-  (** Add a variable into the box. *)
   val extend: t -> R.var_kind -> (t * R.var_id)
-  (** Projection of a variable according to its location in the box. *)
-  val project: t -> R.var_id -> (I.B.t * I.B.t)
-  (** `project_itv box v` projects the interval of the variable `v`. *)
-  val project_itv: t -> R.var_id -> itv
-  (** See `Abstract_domain.lazy_copy`. *)
+  val project: t -> R.var_id -> (B.t * B.t)
   val lazy_copy: t -> int -> t list
-  (** See `Abstract_domain.copy`. *)
   val copy: t -> t
-    * Closure of the store with regards to all constraints in the box.
-      A fixed point is reached when no constraint can be propagated anymore.
-      The entailed constraints are removed from the box.
-      Throw `Bot_found` one of the constraints is unsatisfiable.
   val closure: t -> t
-  (** Propagated and add the constraint `c` into the box.
-      Precondition: The variables in `c` must range over the current store. *)
   val weak_incremental_closure: t -> R.rconstraint -> t
-  (** Return the entailment status of the constraint in `box`. *)
-  val entailment: t -> R.rconstraint -> kleene
-  (** See `Abstract_domain.split`. *)
+  val entailment: t -> R.rconstraint -> Kleene.t
   val split: t -> t list
-  (** Compute the volume of the box.
-      We return `1.` if the box only contain singleton domain. *)
   val volume: t -> float
-  (** Characterize the state of the box.
-      `True` is returned when all the constraints are entailed.
-      `False` is never returned because `Bot_found` is raised in case of unsatisfiability (in `incremental_closure` or `closure`). *)
-  val state_decomposition: t -> kleene
-  (** Print the variables store and the remaining constraints (those not yet entailed). *)
+  val state_decomposition: t -> Kleene.t
   val print: R.t -> Format.formatter -> t -> unit
-  (** See `Var_store.delta`.
-      It returns the variables modified since last call. *)
-  val delta: t -> R.var_id list
 end
 
-module type Box_functor = functor (B: Bound_sig.BOUND) -> Boolean_sat with module I.B = B
-
-module Make
-  (B: Bound_sig.BOUND)
-  (VARDOM: Vardom_sig.Vardom_functor)
-  (STORE: Var_store_functor)
-  (CLOSURE: Hc4.Box_closure_sig)
-  (SPLIT: Box_split.Box_split_sig) : Boolean_sat
-
-module Box_base(SPLIT: Box_split.Box_split_sig) : Box_functor
+module Boolean_sat: Boolean_sat_sig
