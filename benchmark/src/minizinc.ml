5db34246a51b5c9c6c400800763f6fa4a2b67f3f
Pierre Talbot <ptalbot@hyc.io>
2019-07-11 11:08:30 +0200

    [Benchmark][chuffed] Try another fix for Chuffed top level failure.

--- benchmark/src/minizinc.ml
+++ benchmark/src/minizinc.ml
@@ -76,6 +76,9 @@ let mzn_solver_output_to_entries lines =
 let choco_solver_output_to_entries lines =
   eprintf_and_exit "Choco solver is not yet supported."
 
+let top_level_error lines =
+  List.exists (fun l -> (String.trim l) = "% Top level failure!") lines
+
 let solver_output_to_entries (solver: solver_config) output =
   let data = file_to_string output in
   let lines = String.split_on_char '\n' data in
@@ -83,7 +86,7 @@ let solver_output_to_entries (solver: solver_config) output =
   if name = "choco" then
     choco_solver_output_to_entries lines
   (* In this case it is unsatisfiable at top-level, which means that Chuffed does not output any stat, not even time. *)
-  else if name = "chuffed" && List.length lines = 2 then
+  else if name = "chuffed" && top_level_error lines then
     clean_up_entries [("solveTime","0.01")] lines
   else
     mzn_solver_output_to_entries lines
